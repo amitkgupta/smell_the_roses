d9ae3e86bd55a3b3384a0d0a44c97258cc42be5d
Amit Kumar Gupta <amitkgupta84@gmail.com>
2012-07-14 15:13:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmellTheRoses::Application.config.secret_token = 'f1e34aeb39ed4ee1671669ccbb84af3b8a93d71817db1207cf5f9cf2cd2b0cfe2ff108f8de665f0381a73e03cb8fbf2b6853139e439051d8f829a859ed93025e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SmellTheRoses::Application.config.session_store :cookie_store, key: '_smell_the_roses_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SmellTheRoses::Application.config.session_store :active_record_store
