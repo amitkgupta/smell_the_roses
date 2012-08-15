a8c02fe158daaf7a8468c8d15e4bba76c12a10d9
Amit Kumar Gupta <amitkgupta84@gmail.com>
2012-08-15 15:56:33 -0700

    further refactor

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,6 +6,8 @@ var markerPositions;
 var geocoder;
 var directionsRenderer;
 var routeRequest;
+
+var start, end, poi, deviation, travelMode;
 		
 function initialize() {
   	var sanFrancisco = new google.maps.LatLng(37.7750, -122.4183);
@@ -23,6 +25,39 @@ function initialize() {
 	directionsRenderer = new google.maps.DirectionsRenderer({map: map});
 }
 
+function showPOIs() {
+	prepareRouteRequest();							
+	directionsService.route(routeRequest, function(routeResults, status) {
+		var steps = routeResults.routes[0]['legs'][0]['steps'];
+		renderRouteResults(steps, routeResults);
+		buildSearchPoints(steps).forEach(function(point) {
+			addMarkersNearPoint(point);
+		});
+	});
+}
+
+function prepareRouteRequest() {
+	start = document.getElementById("start").value;
+	end = document.getElementById("end").value;
+	poi = document.getElementById("poi").value;
+	deviation = document.getElementById("deviation").value;
+	travelMode = document.getElementById("travelMode").value;
+	routeRequest = {
+		origin: start,
+		destination: end,
+		travelMode: travelMode 
+	}
+}
+
+function renderRouteResults(steps, routeResults) {
+	var bounds = new google.maps.LatLngBounds();
+	bounds.extend(steps[0].start_location);
+	bounds.extend(steps[steps.length-1].end_location);			
+	resetMarkers();
+	directionsRenderer.setDirections(routeResults);
+	resetMap(bounds);
+}
+
 function resetMarkers() {
 	markers.forEach(function(marker) {
 		marker.setMap(null);
@@ -31,7 +66,7 @@ function resetMarkers() {
 	markerPositions = [];
 }
 
-function resetMap(bounds, deviation) {
+function resetMap(bounds) {
 	var south = bounds.getSouthWest().lat()-(deviation/100000);
 	var west = bounds.getSouthWest().lng()-(deviation/100000);
 	var north = bounds.getNorthEast().lat()+(deviation/100000);
@@ -43,44 +78,10 @@ function resetMap(bounds, deviation) {
 	map.fitBounds(bounds);
 }
 
-function showPOIs() {
-	resetMarkers();
-							
-	var start = document.getElementById("start").value;
-	var end = document.getElementById("end").value;
-	var poi = document.getElementById("poi").value;
-	var deviation = document.getElementById("deviation").value;
-	var travelMode = document.getElementById("travelMode").value;
-	
-	routeRequest = {
-		origin: start,
-		destination: end,
-		travelMode: travelMode 
-	}
-	  			
-	directionsService.route(routeRequest, function(routeResults, status) {
-// 		if(status == google.maps.DirectionsStatus.OK) {
-		directionsRenderer.setDirections(routeResults);
-		
-		var steps = routeResults.routes[0]['legs'][0]['steps'];
-
-		var bounds = new google.maps.LatLngBounds();
-		bounds.extend(steps[0].start_location);
-		bounds.extend(steps[steps.length-1].end_location);			
-		resetMap(bounds, deviation);
-
-		buildSearchPoints(steps).forEach(function(point) {
-			addMarkersNearPoint(point, deviation, poi);
-		});
-// 		}
-	});
-}
-
 function buildSearchPoints(steps) {
 	var searchPoints = [];
 	searchPoints.push(steps[0].start_location);
 	searchPoints.push(steps[steps.length-1].end_location);
-	
 	for (i=0; i < steps.length-1; i++) {
 		if (steps[i].travel_mode != "TRANSIT") {
 			searchPoints.push(steps[i].start_location);
@@ -89,25 +90,21 @@ function buildSearchPoints(steps) {
 			}
 		}
 	}
-
 	return searchPoints;
 }
 
-function addMarkersNearPoint(point, deviation, poi) {
+function addMarkersNearPoint(point) {
 	var searchRequest = {
 		location: point,
 		radius: deviation,
 		name: poi
-	}
-							
+	}					
 	placesService.search(searchRequest, function(searchResults, status) {
-// 		if (status == google.maps.places.PlacesServiceStatus.OK) {
 		searchResults.forEach(function(result) {
 			if ($.inArray("result.geometry.location", markerPositions) == -1) {
 				addMarker(result.geometry.location, result.name, result.rating);
 			}
 		});
-// 		}
 	});
 }
 
@@ -117,10 +114,8 @@ function addMarker(location, name, rating) {
 		position: location,
 		title: name.concat(" (rating: ").concat(rating).concat(")")
 	});
-
 	markers.push(marker);
 	markerPositions.push(location);
-		
 	if (routeRequest.travelMode != "TRANSIT") { addListenerToMarker(marker); }
 }
 
@@ -128,9 +123,7 @@ function addListenerToMarker(marker) {
 	google.maps.event.addListener(marker, 'click', function() {
 		routeRequest.waypoints = [{location: marker.getPosition()}]
 		directionsService.route(routeRequest, function(routeResults, status) {
-// 			if(status == google.maps.DirectionsStatus.OK) {
 			directionsRenderer.setDirections(routeResults);
-// 			}
 		});
 	});
 }
